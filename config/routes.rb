bf2dd32e76561190910f736aae5a29ad6d435a75
Alejandro Laserna <alejandro.laserna963@gmail.com>
2013-07-19 11:28:11 -0400

    Sorcery authentication complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Blogger::Application.routes.draw do
 	root "articles#index"
-  resources :articles do
-  	resources :comments
+	resources :articles do
+		resources :comments
 
-  end
-  resources :tags
+	end
+	resources :tags
+	resources :authors
+	resources :author_sessions, only: [ :new, :create, :destroy ]
+
+	match 'login'  => 'author_sessions#new', via: :get
+	match 'logout' => 'author_sessions#destroy', via: :delete
 end
